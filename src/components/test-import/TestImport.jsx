707b72bd5c17b406ba737c6ea03def3c1b4d5f37
Fabien Thebaud <fabien.thebaud@amdocs.com>
2018-04-03 14:44:06 +0200

    import js second example

--- src/components/test-import/TestImport.jsx
+++ src/components/test-import/TestImport.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 import DummyTwo from 'components/common/dummy-two/DummyTwo.jsx';
-import something from 'components/common/myFile';
+import something from 'components/common/my-file/myFile.js';
 
 import DummyOne from '../common/dummy-one/DummyOne.jsx';
+import somethingElse from '../common/my-file/myFile2.js';
 
 import SubComponent from './sub-component/SubComponent.jsx';
 import styles from './TestImport.module.scss';
